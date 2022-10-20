92f464822c624cb2b2019a8e285b63831f501725
Lara Bucher <bucher.lara@web.de>
2022-10-20 20:36:36 +0200

    create Compontents

---
+++ src/Button/Button.js
@@ -0,0 +1,13 @@
+import "./Button.css";
+
+export default function Button() {
+  return (
+    <button
+      className="card__button-answer"
+      type="button"
+      data-js="answer-button"
+    >
+      Show answer
+    </button>
+  );
+}
